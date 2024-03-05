84265ed78e954c14d7ee7e308c1fe5053df6d276
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-03-05 23:12:52 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const day = document.getElementById('day');
-const month = document.getElementById('month');
-const year = document.getElementById('year');
+function computeAge() {
+  const day = document.getElementById("inputDay");
+  const month = document.getElementById("inputMonth");
+  const year = document.getElementById("inputYear");
+
+  if (!day.value) {
+    day.parentElement.classList.add('error');
+    day.nextElementSibling.innerHTML = "This field is required";
+  } else {
+      day.nextElementSibling.innerHTML = "";
+    day.parentElement.classList.remove('error');
+  }
+  if (!month.value) {
+    month.parentElement.classList.add('error');
+    month.nextElementSibling.innerHTML = "This field is required";
+  } else {
+      month.nextElementSibling.innerHTML = "";
+    month.parentElement.classList.remove('error');
+  }
+  if (!year.value) {
+    year.parentElement.classList.add('error');
+    year.nextElementSibling.innerHTML = "This field is required";
+  } else {
+      year.nextElementSibling.innerHTML = "";
+      year.parentElement.classList.remove('error');
+  }
+}
