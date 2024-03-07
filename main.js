7c290dcbd1d21ba2391934c75503b4055377d36d
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-03-07 23:11:35 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
-function computeAge() {
-  const day = document.getElementById("inputDay");
-  const month = document.getElementById("inputMonth");
-  const year = document.getElementById("inputYear");
+let date = new Date();
+let fullDay = date.getDay();
+let fullMonth = date.getMonth()+1;
+let fullYear = date.getFullYear();
+const day = document.getElementById("inputDay");
+const month = document.getElementById("inputMonth");
+const year = document.getElementById("inputYear");
 
-  day.value ? hideError(day) : showError(day);
-  month.value ? hideError(month) : showError(month);
-  year.value ? hideError(year) : showError(year);
-  
-  let date = new Date();
-  let fullDay = date.getDay();
-  let fullMonth = date.getMonth();
-  let fullYear = date.getFullYear();
+function computeAge() {
+  if (!day.value) {
+    showError(day);
+  }else if(day.value > fullDay){
+    day.parentElement.classList.add('error');
+    day.nextElementSibling.innerHTML = 'Must be a valid day';
+  }else{
+    hideError(day);
+  }
+
+  if (!month.value) {
+    showError(month);
+  }else{
+      if(year.value == fullYear){
+        if(month.value > fullMonth || month.value < 1){
+          month.parentElement.classList.add('error');
+          month.nextElementSibling.innerHTML = 'Must be a valid month';
+        }else{
+          hideError(month);
+        }
+      }else if(year.value < fullYear){
+        if(month.value > 12 || month.value < 1){
+          month.parentElement.classList.add('error');
+          month.nextElementSibling.innerHTML = 'Must be a valid month';
+        }else{
+          hideError(month);
+        }
+      }else{
+        hideError(month);
+      }
+  }
+
+  if (!year.value) {
+    showError(year);
+  } else if (year.value > fullYear || year.value < 1) {
+      year.parentElement.classList.add('error');
+      year.nextElementSibling.innerHTML = 'Must be in the past';
+    } else{
+      hideError(year);
+    }
 }
 
-const showError = (getDate) => {
+function showError(getDate) {
   getDate.parentElement.classList.add("error");
   getDate.nextElementSibling.innerHTML = "This field is required";
 }
 
-const hideError = (getDate) => {
+function hideError(getDate) {
   getDate.nextElementSibling.innerHTML = "";
   getDate.parentElement.classList.remove("error");
 }
