677420997f20af6d4522a540607bfaac0a5ba0ad
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-03-08 17:29:51 +0530

    added day error logic

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 let date = new Date();
-let fullDay = date.getDay()+3;
+let fullDay = date.getDate();
 let fullMonth = date.getMonth() + 1;
 let fullYear = date.getFullYear();
 const day = document.getElementById("inputDay");
 const month = document.getElementById("inputMonth");
 const year = document.getElementById("inputYear");
-const numberOfDays = getNumberOfDaysInMonth(year, month);
 
 
 function computeAge() {
-  if (!day.value) {
+  const numberOfDays = getNumberOfDaysInMonth(year.value, month.value);
+
+  if(!day.value){
     showError(day);
-  }else if(day.value > fullDay){
+  }else if((month.value == fullMonth && year.value == fullYear) && (day.value > 0 && day.value <= fullDay)){
+    hideError(day);
+  }else if((day.value > 0 && day.value <= numberOfDays) && ( year.value != fullYear || month.value != fullMonth)){
+    hideError(day);
+  }else{
     day.parentElement.classList.add('error');
     day.nextElementSibling.innerHTML = 'Must be a valid day';
-  }else{
-    hideError(day);
   }
 
   if (!month.value) {
     showError(month);
   }else{
     if((year.value == fullYear && (month.value > fullMonth || month.value < 1))||(year.value < fullYear && (month.value > 12 || month.value < 1))){
-      isvalid(month);
+      Notvalid(month);
     }else{
       hideError(month);
     }
@@ -48,18 +51,12 @@ function hideError(getDate) {
   getDate.parentElement.classList.remove("error");
 }
 
-function isvalid(isValid){
+function Notvalid(isValid){
   isValid.parentElement.classList.add('error');
-  isValid.nextElementSibling.innerHTML = `Must be a valid month`;
+  isValid.nextElementSibling.innerHTML = 'Must be a valid month';
 }
 
 function getNumberOfDaysInMonth(year, month) {
-  let date = new Date(year, month + 1, 0);
+  let date = new Date(year, month, 0);
   return date.getDate();
 }
-
-// code to get date of spacific month.
-// var year = 2024; // Change to the desired year
-//     var month = 5; // Change to the desired month (0-indexed, so 2 is March)
-//     var numberOfDays = getNumberOfDaysInMonth(year, month);
-//     console.log("Number of days in the month:", numberOfDays);
